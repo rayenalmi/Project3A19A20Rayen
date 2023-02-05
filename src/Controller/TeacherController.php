8a9ae2ce162101009ed3f88584d002d81b8538a6
mohamedrayelalmi <rayenalmi64@gmail.com>
2023-02-05 11:58:56 +0100

    start

---
+++ src/Controller/TeacherController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TeacherController extends AbstractController
+{
+    #[Route('/teacher', name: 'app_teacher')]
+    public function index(): Response
+    {
+        return $this->render('teacher/index.html.twig', [
+            'controller_name' => 'TeacherController',
+        ]);
+    }
+
+    #[Route('/gotostudent', name: 'app_teacherGo')]
+    public function GoToStudent()
+    {
+        return $this->redirectToRoute('app_student');
+    }
+}
